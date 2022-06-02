c597be15a500c3bfe20dafae49759d374f3a935a
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-06-01 19:11:38 -0700

    create bootcamp_app content

---
+++ bootcamp_app/students.js
@@ -0,0 +1,49 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "rafaelnobrega",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+
+
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
+
+
+
+// const cohortName = process.argv[2];
+// const limit = process.argv[3]; // maximum number of results
+// let parameters = [`%${cohortName}%`, limit];
+
+// pool
+//   .query(
+//     `
+// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts.name LIKE $1
+// LIMIT $2;
+// `,
+//     parameters
+//   )
+//   .then((res) => {
+//     res.rows.forEach((user) => {
+//       console.log(
+//         `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
+//       );
+//     });
+//   })
+//   .catch((e) => console.error(e.stack));
