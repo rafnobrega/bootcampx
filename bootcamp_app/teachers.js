1dca0e43f33758c4abb48c2c99fa40222e08c049
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-06-01 19:34:11 -0700

    prevent SQL injection at teachers.js

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,17 +8,21 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
+const cohortName = process.argv[2] || 'JUL02';
 
-
-pool.query(`
+const text = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`;
+
+const values = [`%${cohortName}%`];
+
+pool.query(text, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
